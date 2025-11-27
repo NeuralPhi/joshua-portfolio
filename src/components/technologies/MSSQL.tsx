c40cd3af95ba140fc8aaeaca802ee3cd5dfbf8b1
Joshua Opadoja <opadojajoshua@gmail.com>
2025-11-27 22:21:53 +0100

    Create MSSQL.tsx

---
+++ src/components/technologies/MSSQL.tsx
@@ -0,0 +1,15 @@
+export default function MSSQL() {
+  return (
+    <svg viewBox="0 0 128 128">
+      <rect width="128" height="128" rx="16" fill="#CC2927" />
+      <path
+        fill="#fff"
+        d="M32 24h64v16H32v-16zm0 24h64v16H32V48zm0 24h64v16H32v-16zm0 24h64v16H32v-16z"
+      />
+      <path
+        fill="#fff"
+        d="M96 96H32v8h64v-8z"
+      />
+    </svg>
+  );
+}
