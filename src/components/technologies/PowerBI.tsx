d4b6c9318f884d94bb0ef64de002bf563a171d2f
Joshua Opadoja <opadojajoshua@gmail.com>
2025-11-27 21:29:58 +0100

    Create PowerBI.tsx

---
+++ src/components/technologies/PowerBI.tsx
@@ -0,0 +1,15 @@
+export default function PowerBI() {
+  return (
+    <svg viewBox="0 0 256 256">
+      <rect width="256" height="256" fill="none"></rect>
+      <path
+        fill="#F2C811"
+        d="M224 64v128a16 16 0 0 1-16 16H48a16 16 0 0 1-16-16V64a16 16 0 0 1 16-16h160a16 16 0 0 1 16 16Z"
+      ></path>
+      <path
+        fill="#000"
+        d="M88 176a8 8 0 0 1-8-8V88a8 8 0 0 1 16 0v80a8 8 0 0 1-8 8Zm40 0a8 8 0 0 1-8-8V104a8 8 0 0 1 16 0v64a8 8 0 0 1-8 8Zm40 0a8 8 0 0 1-8-8V120a8 8 0 0 1 16 0v48a8 8 0 0 1-8 8Z"
+      ></path>
+    </svg>
+  );
+}
