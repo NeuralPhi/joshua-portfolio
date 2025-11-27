b8148100b688450b1e20e0c0b51f9c57ca96ab07
Joshua Opadoja <opadojajoshua@gmail.com>
2025-11-27 16:25:22 +0100

    Replace Excel icon with updated SVG design

--- src/components/technologies/Excel.tsx
+++ src/components/technologies/Excel.tsx
@@ -1,28 +1,11 @@
 export default function Excel() {
   return (
-    <svg
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      {/* Background rounded square */}
-      <rect width="48" height="48" rx="8" fill="#107C41" />
-
-      {/* Stylized X for Excel */}
-      <text
-        x="50%"
-        y="55%"
-        textAnchor="middle"
-        dominantBaseline="middle"
-        fontSize="24"
-        fontWeight="bold"
-        fill="white"
-        fontFamily="Arial, sans-serif"
-      >
-        X
-      </text>
+    <svg viewBox="0 0 128 128">
+      <rect width="128" height="128" rx="16" fill="#217346" />
+      <path
+        fill="#fff"
+        d="M36 36h12l16 28-16 28H36l16-28-16-28zM64 36h28v12H64v-12zm0 20h28v12H64v-12zm0 20h28v12H64v-12z"
+      />
     </svg>
   );
 }
